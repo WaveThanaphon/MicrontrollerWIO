d6a746f641086075b0044d80e7aced408172b182
Nathee Srina <nathee.sri@lic.ac.th>
2024-11-07 14:29:38 +0700

    Update to using DFRobot MAX30102

--- Hearthrate/Hearthrate.ino
+++ Hearthrate/Hearthrate.ino
@@ -1,9 +1,9 @@
 #include <TFT_eSPI.h>  // Include the graphics library (this includes the sprite functions)
 #include <Wire.h>
-#include <Seeed_HM330X.h>
+#include "DFRobot_MAX30102.h"
 
 TFT_eSPI tft = TFT_eSPI();  // Create object "tft"
-HM330X sensor;
+DFRobot_MAX30102 sensor;
 
 void setup() {
     tft.init();
@@ -13,20 +13,26 @@ void setup() {
     tft.setTextSize(2);
 
     Wire.begin();
-    sensor.init();
+    sensor.begin();
+    sensor.sensorStartCollect();
 }
 
 void loop() {
-    uint8_t buf[30];
-    sensor.read_sensor_value(buf, 29);
-
-    int heartRate = buf[10];  // Assuming heart rate data is at index 10
-
-    tft.fillScreen(TFT_BLACK);
-    tft.setCursor(10, 10);
-    tft.print("Heart Rate: ");
-    tft.print(heartRate);
-    tft.print(" BPM");
+    if (sensor.available()) {
+        uint32_t irValue = sensor.getIR();
+        if (irValue > 50000) {  // Check if a finger is placed on the sensor
+            int heartRate = sensor.getHeartRate();
+            tft.fillScreen(TFT_BLACK);
+            tft.setCursor(10, 10);
+            tft.print("Heart Rate: ");
+            tft.print(heartRate);
+            tft.print(" BPM");
+        } else {
+            tft.fillScreen(TFT_BLACK);
+            tft.setCursor(10, 10);
+            tft.print("No finger detected");
+        }
+    }
 
     delay(1000);  // Update every second
 }
